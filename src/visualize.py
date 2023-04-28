37d3a4ec198b6daecb010c7092cc99af027f3bbb
Tendai Nyamuronda <tnyamuronda26@cmc.edu>
2023-04-27 20:09:19 -0700

    first

--- src/visualize.py
+++ src/visualize.py
@@ -12,7 +12,7 @@ args = parser.parse_args()
 import os
 import json
 from collections import Counter,defaultdict
-
+import matplotlib.pyplot as plt
 # open the input path
 with open(args.input_path) as f:
     counts = json.load(f)
@@ -23,6 +23,21 @@ if args.percent:
         counts[args.key][k] /= counts['_all'][k]
 
 # print the count values
-items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
-for k,v in items:
-    print(k,':',v)
+#items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
+#for k,v in items:
+#   print(k,':',v)
+items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]))[-10:]
+
+# create a bar graph of the results
+keys = [item[0] for item in items]
+values = [item[1] for item in items]
+plt.bar(keys, values)
+
+# set the title and labels of the graph
+plt.title(args.key + ' Counts')
+plt.xlabel('Keys')
+plt.ylabel('Values')
+
+# save the graph as a png file
+output_path = os.path.splitext(args.input_path)[0] + '_' + args.key + '.png'
+plt.savefig(output_path)
